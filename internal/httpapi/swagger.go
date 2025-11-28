29da34ecd80fa5c5852b916e892ba5fa0051be61
Algis Dumbris <a.dumbris@gmail.com>
2025-11-28 18:55:38 +0200

    Serve Swagger UI with 3.1-friendly assets

--- internal/httpapi/swagger.go
+++ internal/httpapi/swagger.go
@@ -1,11 +1,13 @@
 package httpapi
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"go.uber.org/zap"
 
-	httpSwagger "github.com/swaggo/http-swagger"
+	swagv1 "github.com/swaggo/swag"
 	_ "mcpproxy-go/oas" // Import generated docs
 )
 
@@ -13,5 +15,59 @@ import (
 // This is exported so it can be mounted on the main mux
 func SetupSwaggerHandler(logger *zap.SugaredLogger) http.Handler {
 	logger.Debug("Setting up Swagger UI handler")
-	return httpSwagger.WrapHandler
+	return &swaggerHandler{logger: logger}
 }
+
+type swaggerHandler struct {
+	logger *zap.SugaredLogger
+}
+
+func (h *swaggerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/swagger/")
+
+	switch path {
+	case "", "/":
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		_, _ = fmt.Fprint(w, swaggerHTML)
+	case "doc.json":
+		doc, err := swagv1.ReadDoc()
+		if err != nil {
+			h.logger.Errorf("failed to read swagger doc: %v", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		_, _ = w.Write([]byte(doc))
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+const swaggerHTML = `<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Swagger UI</title>
+  <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui.css" />
+  <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist@5.17.14/favicon-32x32.png" sizes="32x32" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.17.14/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = () => {
+      window.ui = SwaggerUIBundle({
+        url: "/swagger/doc.json",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout"
+      });
+    };
+  </script>
+</body>
+</html>`
--- oas/register_compat.go
+++ oas/register_compat.go
@@ -2,7 +2,7 @@ package oas
 
 import swagv1 "github.com/swaggo/swag"
 
-// register generated spec with swag v1 so http-swagger (which depends on v1 path) can serve doc.json
+// register generated spec with swag v1 so our doc.json handler can read from the v1 registry
 func init() {
 	// swagger UI handler reads from swag v1 registry; we reuse the v2-generated spec since it implements ReadDoc.
 	swagv1.Register(SwaggerInfo.InstanceName(), SwaggerInfo)
